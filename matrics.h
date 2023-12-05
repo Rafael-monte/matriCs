aae81baa7df8f4d22182382f7364b6b72079d7d7
Rafael-monte <rafaelmonte46@gmail.com>
2023-12-05 14:33:33 -0300

    Added some fixes and docs about functions

--- matrics.h
+++ matrics.h
@@ -5,33 +5,50 @@
 #include <stdio.h>
 
 //Constants
-#ifndef DEFAULT_FLOAT_VALUE
+#ifndef DEFAULT_DOUBLE_VALUE
     #define DEFAULT_DOUBLE_VALUE 0.0
 #endif
+#ifndef DEFAULT_VEC_ROW_R_SIZE
+    #define DEFAULT_VEC_ROW_R_SIZE 1
+#endif
+#ifndef DEFAULT_VEC_COL_C_SIZE
+    #define DEFAULT_VEC_COL_C_SIZE 1
+#endif
 
-typedef enum MATRIX_TYPE {
+typedef enum M_TYPE {
     IDENTITY = 0,
     CUSTOM = 1,
     VECTOR_ROW = 2,
     VECTOR_COL = 3
-} MATRIX_TYPE;
+} M_TYPE;
 
 
 typedef struct MATRIX {
     double** content;
-    int rows;
-    int cols;
-    MATRIX_TYPE matrix_type;
+    size_t rows;
+    size_t cols;
+    M_TYPE matrix_type;
 } MATRIX;
 
-MATRIX CreateIdentity(const unsigned int scale)
+/* 
+    Creates a identity (I) matrix given the scale of it.
+    i.e:
+    MATRIX m = CreateIdentity(3);
+    m.content will be:
+    [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ]
+*/
+MATRIX CreateIdentity(const size_t scale)
 {
     double **m_content = (double **) malloc(sizeof(double) * scale);
     if (!m_content) 
     {
         fprintf(stderr, "Cannot allocate matrix content.\n");
     }
-    for (int i = 0; (unsigned int) i < scale; ++i)
+    for (size_t i = 0; i < scale; ++i)
     {
         m_content[i] = (double *) malloc(sizeof(double) * scale);
         if (!m_content[i]) 
@@ -48,66 +65,151 @@ MATRIX CreateIdentity(const unsigned int scale)
     return res;
 }
 
+/*
+    Deallocates the matrix content from heap.
+    It's crucial do that when the given matrix its no more useful
+    i.e:
+        MATRIX m = CreateIdentity(2);
+        Deallocate(&m);
+        Now m.content will be NULL
+*/
 void Deallocate(MATRIX* matrix)
 {
-    for (int i = 0; i < matrix->rows; ++i)
+    for (size_t i = 0; i < matrix->rows; ++i)
     {
         free(matrix->content[i]);
+        matrix->content[i] = NULL;
+    }
+    free(matrix->content);
+    matrix->content = NULL;
+}
+
+void __print_vector_col(MATRIX* m)
+{
+    puts("[");
+    for (size_t i = 0; i < m->rows; ++i)
+    {
+        printf(i == m->rows - 1? "  %.2lf": "  %.2lf,\n", m->content[i][0]);
+    }
+    puts("\n]");
+}
+
+void __print_vector_row(MATRIX* m)
+{
+    printf("[ ");
+    for (size_t i = 0; i < m->cols; ++i)
+    {
+        printf(i == m->cols - 1? "%.2lf ]": "%.2lf, ", m->content[i][0]);
     }
 }
 
-void PrintMatrix(MATRIX *matrix, bool consumeAfter)
+void __print_bidimensional_matrix(MATRIX* m)
 {
     puts("[");
-    for (int i = 0; i < matrix->rows; ++i)
+    for (size_t i = 0; i < m->rows; ++i)
     {
-        printf("  [");
-        for (int j = 0; j < matrix->cols; ++j)
+        printf("  [ ");
+        for (size_t j = 0; j < m->cols; ++j)
         {
-            printf("%.2lf, ", matrix->content[i][j]);
+            printf(j == m->cols - 1?"%.2lf ": "%.2lf, ", m->content[i][j]);
         }
         printf("]\n");
     }
     puts("]");
+}
+
+/*
+    Prints the matrix in the stdout.
+    The 'consumeAfter' param means if the matrix should be deallocated after the operation succeed.
+*/
+void PrintMatrix(MATRIX *matrix, bool consumeAfter)
+{
+
+    switch (matrix->matrix_type) {
+        case VECTOR_COL:
+            __print_vector_col(matrix);
+            break;
+        case VECTOR_ROW:
+            __print_vector_row(matrix);
+            break;
+        case CUSTOM:
+        case IDENTITY:
+            __print_bidimensional_matrix(matrix);
+            break;
+    }
     if (consumeAfter)
     {
         Deallocate(matrix);
     }
 }
 
+/*
+    Creates an vector row given the row size.
+    ie:
+    MATRIX vecRow = CreateVectorRow(5);
+    PrintMatrix(&vecRow, false);
+    It will output:
+    [ 0.00, 0.00, 0.00, 0.00, 0.00 ]
+*/
 MATRIX CreateVectorRow(size_t rowSize)
 {
-    double** row = (double **) malloc(sizeof(double *) * rowSize);
+    double** row = (double **) malloc(sizeof(double *));
     for (size_t i = 0; i < rowSize; ++i)
     {
-        row[i] = (double *) malloc(sizeof(double));
-        row[i][0] = DEFAULT_DOUBLE_VALUE;
+        row[i] = (double *) malloc(sizeof(double) * rowSize);
     }
     
     MATRIX res;
     res.content = row;
-    res.rows = 1;
+    res.rows = DEFAULT_VEC_ROW_R_SIZE;
     res.cols = rowSize;
     res.matrix_type = VECTOR_ROW;
     return res;
 }
 
+/*
+    Creates an vector column given the column size.
+    ie:
+    MATRIX vecCol = CreateVectorCol(5);
+    PrintMatrix(&vecCol, false);
+    It will output:
+    [
+      0.00,
+      0.00,
+      0.00,
+      0.00,
+      0.00
+    ]
+*/
 MATRIX CreateVectorCol(size_t colSize)
 {
-    double** row = (double **) malloc(sizeof(double *));
-    row[0] = (double *) malloc(sizeof(double) * colSize);
+    double** rows = (double **) malloc(sizeof(double *) * colSize);
+
     for (size_t i = 0; i < colSize; ++i)
     {
-        row[0][i] = DEFAULT_DOUBLE_VALUE;
+        rows[i] = (double *) malloc(sizeof(double *));
+        rows[i][0] = DEFAULT_DOUBLE_VALUE;
     }
     MATRIX m;
-    m.rows = 1;
-    m.cols = colSize;
-    m.content = row;
+    m.rows = colSize;
+    m.cols = DEFAULT_VEC_COL_C_SIZE;
+    m.content = rows;
     m.matrix_type = VECTOR_COL;
     return m;
 }
 
+
+/*
+    Creates a matrix with custom row size and column size.
+    ie:
+    MATRIX custom = CreateCustomMatrix(2, 3);
+    PrintMatrix(&custom, false);
+    It will output:
+    [
+        0.00, 0.00, 0.00,
+        0.00, 0.00, 0.00
+    ]
+*/
 MATRIX CreateCustomMatrix(size_t rowSize, size_t colSize)
 {
     double** row = (double **) malloc(sizeof(double *) * rowSize);
